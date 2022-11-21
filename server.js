1119ed2aeba9a1d00faf2a3805b6fa660c1c1c53
MiguelDiaz <madiaz@escuelasproa.edu.ar>
2022-11-21 18:52:59 -0300

    base

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser') 
+const port = 2500
+
+app.use(bodyParser.json());
+
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port} correctamente`)
+})
